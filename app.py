3f70062673ec78cf8c358ea363db4b3937ca714e
Dev Khant <devkhant24@gmail.com>
2023-08-15 15:10:40 +0530

    add streamlit app

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from process.video_audio_explanation import Explain
+
+st.title("Video Understanding and Q&A Tool")
+
+video_link = st.text_input("Enter Youtube Video Link")
+
+huggingface_token = st.text_input("Enter Hugging Face Token")
+
+if st.button("Explain"):
+    if video_link and huggingface_token:
+        get_explanation = Explain(video_link, huggingface_token)
+        result_text = get_explanation.run()
+
+        st.text_area("Processed Text", result_text, height=200)
+    else:
+        st.warning("Please provide both the YouTube video link and Hugging Face Token.")
---
+++ process/video_audio_explanation.py
@@ -0,0 +1,7 @@
+class Explain:
+    def __init__(self, yt_link, hf_token):
+        self.yt_link = yt_link
+        self.hf_token = hf_token
+
+    def run(self):
+        pass
