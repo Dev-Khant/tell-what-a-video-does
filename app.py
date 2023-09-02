a717e4c29e152f1a50df94caba58942e7702e8dc
Dev Khant <devkhant24@gmail.com>
2023-09-02 19:51:13 +0530

    Add openai key

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ st.title("Video Understanding and Q&A Tool")
 
 video_link = st.text_input("Enter Youtube Video Link")
 
+openai_token = st.text_input("Enter OpenAI Token")
+
 huggingface_token = st.text_input("Enter Hugging Face Token")
 
 serpapi_token = st.text_input("Enter SerpAPI Token")
@@ -13,7 +15,9 @@ serpapi_token = st.text_input("Enter SerpAPI Token")
 if st.button("Explain"):
     with st.spinner("Loading..."):
         if video_link and huggingface_token:
-            get_explanation = Explain(video_link, huggingface_token, serpapi_token)
+            get_explanation = Explain(
+                video_link, openai_token, huggingface_token, serpapi_token
+            )
             result_text = get_explanation.run()
 
             st.text_area("Processed Text", "result_text", height=200)
--- process/video_audio_explanation.py
+++ process/video_audio_explanation.py
@@ -1,10 +1,12 @@
+from process.llm import SummaryGeneration
 from process.video.explain_video import VideoExplanation
 from process.audio.transcribe_audio import GenerateTranscription
 
 
 class Explain:
-    def __init__(self, yt_link, HF_KEY, SERP_KEY):
+    def __init__(self, yt_link, OPENAI_KEY, HF_KEY, SERP_KEY):
         self.yt_link = yt_link
+        self.OPENAI_KEY = OPENAI_KEY
         self.HF_KEY = HF_KEY
         self.SERP_KEY = SERP_KEY
 
@@ -16,6 +18,12 @@ class Explain:
         # for audio
         audio_part = GenerateTranscription(self.yt_link, self.HF_KEY)
         audio_transcription = audio_part.process_audio()
+
+        # merge both video and audio
+        llm_part = SummaryGeneration(
+            self.OPENAI_KEY, video_explanation, audio_transcription
+        )
+        complete_explanation = llm_part.run()
 
         print("Video Explanation : ", video_explanation)
         print("Audio Part : ", audio_transcription)
