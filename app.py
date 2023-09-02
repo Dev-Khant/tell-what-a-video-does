141da2fcc188629d5bd3a97a4637be963d74b5cd
Dev Khant <devkhant24@gmail.com>
2023-09-02 19:05:58 +0530

    formatting

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from process.video_audio_explanation import Explain
+from process.llm import LLMGeneration
 
 st.title("Video Understanding and Q&A Tool")
 
@@ -10,10 +11,13 @@ huggingface_token = st.text_input("Enter Hugging Face Token")
 serpapi_token = st.text_input("Enter SerpAPI Token")
 
 if st.button("Explain"):
-    if video_link and huggingface_token:
-        get_explanation = Explain(video_link, huggingface_token, serpapi_token)
-        result_text = get_explanation.run()
+    with st.spinner("Loading..."):
+        if video_link and huggingface_token:
+            get_explanation = Explain(video_link, huggingface_token, serpapi_token)
+            result_text = get_explanation.run()
 
-        st.text_area("Processed Text", result_text, height=200)
-    else:
-        st.warning("Please provide both the YouTube video link and Hugging Face Token.")
+            st.text_area("Processed Text", "result_text", height=200)
+        else:
+            st.warning(
+                "Please provide both the YouTube video link and Hugging Face Token."
+            )
